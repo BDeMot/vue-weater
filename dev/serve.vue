051a822d10e591b5732c219d0351b2399aa81ff9
Benjamin De Mot <benjamindm@hotmail.fr>
2021-09-03 18:15:03 +0200

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueWeatherSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueWeatherSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-weather-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueWeatherSample } from './vue-weather-sample.vue';
